5dbf76d16611b0b81d38b0f3bf4eb75f4b59ad67
metsingh20 <metsingelias@gmail.com>
2026-02-18 14:10:54 +0200

    Setup custom User model with Admin and Client roles and verified via Django Admin

--- backend/django_core/core/settings.py
+++ backend/django_core/core/settings.py
@@ -51,11 +51,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devtechsociety_db',
+        'USER': 'postgres',
+        'PASSWORD': 'thembi6367@@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
